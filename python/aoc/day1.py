06db42da60f20f6d71e2e5d69e9973369106ff9c
William Mathewson <will@sumdog.com>
2017-12-01 15:05:06 +0000

    Add Python solution for part 1 of day 1

---
+++ python/aoc/day1.py
@@ -0,0 +1,23 @@
+def reduce(number_string: str) -> int:
+    """Counts repeating numbers"""
+    first_num: int = int(number_string[0])
+    prev_num: int = None
+    curr_num: int = None
+    total: int = 0
+
+    for num in number_string:
+        prev_num = curr_num
+        curr_num = int(num)
+        if prev_num == curr_num:
+            total += curr_num
+
+    if curr_num == first_num:
+        total += curr_num
+
+    return total
+
+
+if __name__ == "__main__":
+    string = input("Captcha sequence: ")
+
+    print(reduce(string))
