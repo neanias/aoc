55b9e1d20cd85056a1934a3bf4aaba2131ca956e
William Mathewson <will@sumdog.com>
2017-12-01 17:26:58 +0000

    Complete part 2 in Python

--- python/aoc/day1.py
+++ python/aoc/day1.py
@@ -1,13 +1,17 @@
-def part1(number_string: str) -> int:
+from typing import List
+
+
+def part_1(number_string: str) -> int:
     """Counts repeating numbers"""
-    first_num: int = int(number_string[0])
+    numbers: List[int] = listify(number_string)
+    first_num: int = numbers[0]
     prev_num: int = None
     curr_num: int = None
     total: int = 0
 
-    for num in number_string:
+    for num in numbers:
         prev_num = curr_num
-        curr_num = int(num)
+        curr_num = num
         if prev_num == curr_num:
             total += curr_num
 
@@ -17,7 +21,33 @@ def part1(number_string: str) -> int:
     return total
 
 
+def part_2(number_string: str) -> int:
+    numbers: List[int] = listify(number_string)
+    total: int = 0
+    for i in range(len(numbers)):
+        oppo: int = paired_number(numbers, i)
+        if numbers[i] == numbers[oppo]:
+            total += numbers[i]
+
+    return total
+
+
+def paired_number(numbers: List[int], index: int) -> int:
+    step = len(numbers) // 2
+    oppo = index + step
+    return oppo % len(numbers)
+
+
+def listify(number_string: str) -> List[int]:
+    int_string: List[int] = []
+    for num in number_string:
+        int_string.append(int(num))
+
+    return int_string
+
+
 if __name__ == "__main__":
     string = input("Captcha sequence: ")
 
-    print(part1(string))
+    print(part_1(string))
+    print(part_2(string))
