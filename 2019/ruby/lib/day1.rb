861096e11b2c7996df2267a16ab988207a6822ca
William Mathewson <wncmathewson@me.com>
2019-12-02 21:55:59 +0000

    Add in 2019 code

---
+++ 2019/ruby/lib/day1.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Day1
+  def process_input(file)
+    @masses = File.readlines(file, chomp: true).map(&:to_i)
+  end
+
+  def part_1
+    @masses.sum { |mass| calculate_fuel_needed(mass) }
+  end
+
+  def part_2
+    @masses.sum do |mass|
+      main_fuel = calculate_fuel_needed(mass)
+      self_fuel = main_fuel
+      until self_fuel < 0
+        self_fuel = calculate_fuel_needed(self_fuel)
+        main_fuel += self_fuel if self_fuel > 0
+      end
+
+      main_fuel
+    end
+  end
+
+  private
+
+  def calculate_fuel_needed(mass)
+    (mass / 3).floor - 2
+  end
+end
---
+++ 2019/ruby/spec/day1_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'pathname'
+require 'day1'
+
+RSpec.describe Day1 do
+  subject(:day_1) { described_class.new }
+
+  before do
+    day_1.process_input(generate_fixture_path("day1_input"))
+  end
+
+  it 'calculates how much fuel is required' do
+    expect(day_1.part_1).to eq(34_241)
+  end
+
+  it 'calculates extra fuel as well' do
+    expect(day_1.part_2).to eq(51_316)
+  end
+
+  def generate_fixture_path(filename)
+    File.expand_path(File.join("spec", "fixtures", filename + ".txt"))
+  end
+end
