1b37adabd147f856854e7b4887ebed84c66709cc
William Mathewson <wncmathewson@me.com>
2020-12-09 14:23:06 +0000

    Add day 9 solutions
    Apparently I also missed day 8's specs

---
+++ 2020/ruby/lib/day9.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Day9
+  def initialize(numbers_file, preamble_size = 25)
+    @numbers = File.readlines(numbers_file, chomp: true).map(&:to_i)
+    @preamble_size = preamble_size
+  end
+
+  def part_1
+    @numbers.slice(@preamble_size..).lazy.with_index.find do |n, idx|
+      @numbers.slice(idx, @preamble_size + idx).combination(2).lazy.find { |a, b| a + b == n }.nil?
+    end[0]
+  end
+
+  def part_2(goal)
+    last_index = nil
+    first_index = (0..@numbers.size).lazy.find do |idx|
+      found = search(idx, goal)
+      last_index = found - 1 if found
+    end
+
+    @numbers[first_index..last_index].sort.minmax.sum
+  end
+
+  private
+
+  def search(idx, goal, acc = 0)
+    return idx if acc == goal
+    return nil if acc > goal
+    num = @numbers[idx]
+    new_index = idx + 1
+    search(new_index, goal, acc + num)
+  end
+end
--- 2020/ruby/spec/day8_spec.rb
+++ 2020/ruby/spec/day8_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Day8 do
       expect(day_8.part_1).to eq(5)
     end
   end
+
+  describe "#part_2" do
+    it "finds the answer for the swapped op" do
+      expect(day_8.part_2).to eq(8)
+    end
+  end
 end
