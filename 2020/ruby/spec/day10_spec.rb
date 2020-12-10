4583a8e3a100b9cb62ec78b8eac49d5852b398db
William Mathewson <wncmathewson@me.com>
2020-12-10 16:03:16 +0000

    Add provisional day 10 solution

---
+++ 2020/ruby/spec/day10_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "day10"
+
+RSpec.describe Day10 do
+  let(:problem_input) { generate_input_path(described_class.name.downcase) }
+  subject(:day_10) { described_class.new(problem_input) }
+
+  describe "#part_1" do
+    it "calculates the difference in joltages" do
+      expect(day_10.part_1).to eq(220)
+    end
+  end
+
+  describe "#part_1" do
+    it "calculates the number of arrangements" do
+      expect(day_10.part_2).to eq(19208)
+    end
+  end
+end
