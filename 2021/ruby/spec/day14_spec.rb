79fc51bab35a18630a0e91198061d09343f7d590
Will Mathewson <william.mathewson@freeagent.com>
2021-12-14 17:12:51 +0000

    Add day 14 part 1 solution
    With help from Max!

---
+++ 2021/ruby/spec/day14_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "day14"
+require "spec_helper"
+
+describe Day14 do
+  subject(:day_fourteen) { described_class.new(problem_input) }
+
+  let(:problem_input) { generate_input_path(described_class.name.downcase) }
+
+  describe "#part_one" do
+    it "subtracts the largest and smallest" do
+      expect(day_fourteen.part_one).to eq(1588)
+    end
+  end
+end
