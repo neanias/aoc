bc1388e0dc60b1512d7f68fa30c1959ab45d5ccc
Will Mathewson <william.mathewson@freeagent.com>
2022-12-06 09:28:18 +0000

    Add Sorbet types to day 6

--- 2022/ruby/lib/day6.rb
+++ 2022/ruby/lib/day6.rb
@@ -1,21 +1,28 @@
 # typed: true
 # frozen_string_literal: true
 
+require "sorbet-runtime"
+
 class Day6
+  extend T::Sig
+
   def initialize(signal_file)
     @signal = File.read(signal_file).strip
   end
 
+  sig { returns(Integer) }
   def part_one
     find_marker(4)
   end
 
+  sig { returns(Integer) }
   def part_two
     find_marker(14)
   end
 
   private
 
+  sig { params(size: Integer).returns(Integer) }
   def find_marker(size)
     count = 0
     @signal.chars.each_cons(size).each do |substr|
