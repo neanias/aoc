d3c6f86d1fe12324d0c3d75910ba7953f85c267e
William Mathewson <wncmathewson@me.com>
2022-12-06 20:01:42 +0000

    Add types to day 4

--- 2022/ruby/lib/day4.rb
+++ 2022/ruby/lib/day4.rb
@@ -1,26 +1,32 @@
 # typed: true
 # frozen_string_literal: true
 
-require "CSV"
+require "csv"
+require "sorbet-runtime"
 
 class Day4
+  extend T::Sig
+
+  sig { params(zones_file: String).void }
   def initialize(zones_file)
-    @zones = CSV.readlines(zones_file).map do |(elf1, elf2)|
+    @zones = T.let(CSV.readlines(zones_file).map do |(elf1, elf2)|
       elf1_range = Range.new(*elf1.split("-").map(&:to_i))
       elf2_range = Range.new(*elf2.split("-").map(&:to_i))
       [elf1_range, elf2_range]
-    end
+    end, T::Array[[T::Range[Integer], T::Range[Integer]]])
   end
 
+  sig { returns(Integer) }
   def part_one
     @zones.count do |(elf1_range, elf2_range)|
       elf1_range.cover?(elf2_range) || elf2_range.cover?(elf1_range)
     end
   end
 
+  sig { returns(Integer) }
   def part_two
     @zones.count do |(elf1, elf2)|
-      elf1.to_a.intersect?(elf2.to_a)
+      !(elf1.to_a & elf2.to_a).empty?
     end
   end
 end
