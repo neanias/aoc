37508dbc978471c6826855979e97a9472f9b8c47
Will Mathewson <william.mathewson@freeagent.com>
2022-12-08 12:40:59 +0000

    Add Sorbet types to day 8 soln

--- 2022/ruby/lib/day8.rb
+++ 2022/ruby/lib/day8.rb
@@ -8,18 +8,20 @@ class Day8
   extend T::Sig
 
   def initialize(map_file)
-    @map = Matrix[*File.readlines(map_file, chomp: true).map { _1.chars.map(&:to_i) }]
+    @map = T.let(Matrix[*File.readlines(map_file, chomp: true).map { _1.chars.map(&:to_i) }], Matrix)
   end
 
   def part_one
-    visible_trees = ((@map.row_size + @map.column_size) * 2) - 4 # take off the corners
-    inner_indices = 1..(@map.row_size - 2)
+    # Take away 4 to not double count the corners
+    visible_trees = T.let(((@map.row_count + @map.column_count) * 2) - 4, Integer)
+    inner_indices = T.let(1..(@map.row_count - 2), T::Range[Integer])
     inner_indices.each do |i|
       inner_indices.each do |j|
-        tree = @map[i, j]
-        visible = (
+        tree = T.let(@map[i, j], Integer)
+        visible = T.let(
           @map.row(i)[...j].max < tree || @map.row(i)[j+1..].max < tree ||
-          @map.column(j)[...i].max < tree || @map.column(j)[i+1..].max < tree
+          @map.column(j)[...i].max < tree || @map.column(j)[i+1..].max < tree,
+          T::Boolean
         )
         visible_trees += 1 if visible
       end
@@ -32,21 +34,24 @@ class Day8
     scenic_score = 0
 
     @map.each_with_index do |tree, i, j|
-      row = @map.row(i)
-      column = @map.column(j)
+      row = T.let(@map.row(i), Vector)
+      column = T.let(@map.column(j), Vector)
 
       left = calculate_score(row[...j].reverse, tree)
       right = calculate_score(row[j+1..], tree)
       up = calculate_score(column[...i].reverse, tree)
       down = calculate_score(column[i+1..], tree)
 
-      score = T.let(left * right * up * down, Integer)
+      score = left * right * up * down
       scenic_score = score if score > scenic_score
     end
 
     scenic_score
   end
 
+  private
+
+  sig { params(sight_line: T.any(Vector, T::Array[Integer]), tree: Integer).returns(Integer) }
   def calculate_score(sight_line, tree)
     # Using `take_while { _1 <= tree }` would look past the blocking tree if
     # it's the same height as the current tree.
