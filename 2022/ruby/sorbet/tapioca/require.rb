f2afd1ba24ed66d3674c5e96437506e9d911030a
Will Mathewson <william.mathewson@freeagent.com>
2022-12-08 12:35:38 +0000

    Add Matrix gem Sorbet types

--- 2022/ruby/sorbet/tapioca/require.rb
+++ 2022/ruby/sorbet/tapioca/require.rb
@@ -1,4 +1,6 @@
 # typed: true
 # frozen_string_literal: true
 
-# Add your extra requires here (`bin/tapioca require` can be used to boostrap this list)
+require "csv"
+require "matrix"
+require "sorbet-runtime"
