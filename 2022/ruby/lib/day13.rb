cbe6c2ba8be953ce61fd159ba146331806aaf5fa
Will Mathewson <william.mathewson@freeagent.com>
2022-12-19 11:45:34 +0000

    Add day 13 part two solution

--- 2022/ruby/lib/day13.rb
+++ 2022/ruby/lib/day13.rb
@@ -2,7 +2,6 @@
 # frozen_string_literal: true
 
 require "json"
-require "sorbet-runtime"
 
 class Day13
   def initialize(packets_file)
@@ -12,15 +11,15 @@ class Day13
   end
 
   def part_one
-    @packets.each_with_index.sum do |(packets, idx)|
-      next 0 unless correct_order?(*packets)
+    @packets.each_with_index.sum do |((left, right), idx)|
+      next 0 unless correct_order?(left, right)
 
       idx + 1
     end
   end
 
   def part_two
-    correct_orders = @packets.map { correct_order?(*_1) }
+    correct_orders = @packets.map { |(left, right)| correct_order?(left, right) }
     swapped_packets = correct_orders.flat_map.with_index do |no_swap, idx|
       if no_swap
         @packets[idx]
@@ -29,7 +28,9 @@ class Day13
         [lhs, rhs]
       end
     end
-    sorted_packets = [*swapped_packets, [[2]], [[6]]].sort_by(&:flatten)
+
+    swapped_packets.concat([[[2]]], [[[6]]])
+    sorted_packets = swapped_packets.sort { |a, b| correct_order?(a, b) ? -1 : 1 }
     two_index = sorted_packets.index([[2]]) + 1
     six_index = sorted_packets.index([[6]]) + 1
 
@@ -38,38 +39,30 @@ class Day13
 
   private
 
-  def correct_order?(left, right, indent = 0)
-    # indent_spaces = " " * indent
-    # puts "#{indent_spaces}- Compare #{left} vs #{right}"
-
+  def correct_order?(left, right)
     case [left, right]
     in [nil, *]
-      # puts "#{indent_spaces}true"
       true
     in [_, nil]
-      # puts "#{indent_spaces}false"
       false
     in [Integer, Integer]
       if left == right
         nil
       else
-        # puts "#{indent_spaces}#{left < right}"
         left < right
       end
     in [Array, Array]
       if left.empty? && !right.empty?
-        # puts "#{indent_spaces}true"
         true
       elsif !left.empty? && right.empty?
-        # puts "#{indent_spaces}false"
         false
       else
-        zip(left, right).flat_map { |(a, b)| correct_order?(a, b, indent + 2) }.find { !_1.nil? }
+        zip(left, right).flat_map { |(a, b)| correct_order?(a, b) }.find { !_1.nil? }
       end
     in [Integer, Array]
-      correct_order?([left], right, indent + 2)
+      correct_order?([left], right)
     in [Array, Integer]
-      correct_order?(left, [right], indent + 2)
+      correct_order?(left, [right])
     end
   end
 
