8db944e5523a834e483baccb03dd754f0e286e71
William Mathewson <wncmathewson@me.com>
2023-12-01 11:12:27 +0000

    Add day 1 solution

---
+++ 2023/ruby/spec/day1_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "day1"
+
+RSpec.describe Day1 do
+  subject(:day_one) { described_class.new(problem_input) }
+
+  let(:problem_input) { generate_input_path("day1") }
+
+  describe "#part_one" do
+    subject(:part_one) { day_one.part_one }
+
+    it { is_expected.to eq(142) }
+  end
+
+  describe "#part_two" do
+    subject(:part_two) { day_one.part_two }
+
+    let(:problem_input) { generate_input_path("day1_part2") }
+
+    it { is_expected.to eq(281) }
+  end
+end
